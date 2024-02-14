551e4ffa3cc1afadfc091bf2e4e00bfea24dc94b
kiogimwenda <151431879+kiogimwenda@users.noreply.github.com>
2024-02-14 13:54:27 +0300

    First commit
    Created new repository for structural programming unit codes

---
+++ calc.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("The sum of the two numbers is: %d\n", a + b);
+    printf("The difference of the two numbers is: %d\n", a - b);
+
+    return 0;
+}
